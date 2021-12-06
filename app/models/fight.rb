a5cfa6270ed6e224ce8cde8a17f24a7ea575a541
= <pierreambroise@hotmail.fr>
2021-12-06 10:35:56 +0900

    form for gears to equip

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -11,7 +11,7 @@ class Fight < ApplicationRecord
       red_fighter.id => red_fighter.level,
       blue_fighter.id => blue_fighter.level
     }
-    @player = [red_fighter, blue_fighter].sample
+    @player = [red_fighter, blue_fighter].sample # faster amongst the 2 players for first round
     @opponent = (@player == red_fighter) ? blue_fighter  : red_fighter
   end
 
@@ -21,16 +21,27 @@ class Fight < ApplicationRecord
     @player.gear_stats_array = []
     @opponent.gear_stats_array = []
     while @players_health[@player.id] >= 0
-      # if @player.speed_attack - @opponent.speed_attack < @opponent.speed_attack
-      attack_defence_difference = @player.attack_with_gear - @opponent.defence_with_gear
-      attack_defence_difference = 1 if attack_defence_difference <= 1
-        @players_health[@opponent.id] = @players_health[@opponent.id] - attack_defence_difference
-        self.turns << "#{@player.name} attacks, #{@opponent.name} looses #{attack_defence_difference}Hp, #{@players_health[@opponent.id]}Hp left for #{@opponent.name}."
-        switch_player
-      # end
+      damage = @player.attack_with_gear - @opponent.defence_with_gear
+      damage = 1 if damage <= 1
+        @players_health[@opponent.id] = @players_health[@opponent.id] - damage
+        self.turns << "#{@player.name} attacks, #{@opponent.name} looses #{damage}Hp, #{@players_health[@opponent.id]}Hp left for #{@opponent.name}."
+        switch_player # if speed method says so
     end
     win_declaration
   end
+
+  # def players_speed(red_fighter, blue_fighter)
+
+  #   red_speed = 0 #5
+  #   blue_speed = 0 #2
+  #   red_speed += 2
+
+  #   if red_speed == (blue_speed * red_speed)
+  #   blue_speed += 5
+  #   end
+
+  #   return the highest speed
+  # end
 
   def win_declaration
     self.winner = @opponent.name # tell who the winner is
