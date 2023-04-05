c918e8214e2c2b78d857295d766d89af6b5b416a
= <pierreambroise@hotmail.fr>
2023-04-05 16:48:18 +0200

    added firebase

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,28 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDXYs_UpPCeQ0W30PQ4POj1Ka1WhpQB8ek",
+  authDomain: "fighter-fighter.firebaseapp.com",
+  projectId: "fighter-fighter",
+  storageBucket: "fighter-fighter.appspot.com",
+  messagingSenderId: "245986132908",
+  appId: "1:245986132908:web:4f5f99df5a7e5947947009",
+  measurementId: "G-VYXN5YTTG7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
